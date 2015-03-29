e7f291a8ed9f2a096adf81821d299ca16d2f9d75
Selaginella_V <veronica9907@sina.com>
2015-03-29 16:38:18 -0400

    Update assoc_ibc.sh
    @teacheRforV: 其实最后还是不能正常运行.sh，因为plink生成的中间名字不是正常名字（名字很奇怪），无法进行后续分析，一行一行在终端是可以正常运行的。另外，notepad++修改EOL Conversion为UNIX可以运行：）

--- assoc_ibc.sh
+++ assoc_ibc.sh
@@ -8,8 +8,8 @@ plink --bfile IBCwhole --missing --mind 0.05 --geno 0.05 --maf 0.01 --hwe 0.0000
 ## .bed convert to .ped in order to devide the group into males and females
 plink --bfile IBCqc --recode --out IBCqc
 ## seperate two gender groups, column 5 is sex coding, sex=1 is male, sex=2 is female
-$ awk '$5==2 {print}' IBCqc.ped > IBCqc_f.ped 
-$ awk '$5==1 {print}' IBCqc.ped > IBCqc_m.ped 
+awk '$5==2 {print}' IBCqc.ped > IBCqc_f.ped 
+awk '$5==1 {print}' IBCqc.ped > IBCqc_m.ped 
 
 ## association analysis in females and males, different .ped with the same map
 ## phenotype lifespan is coded as binary variable, normally coded as control = 1 (age<85), case = 2 (age>92). 
@@ -21,7 +21,7 @@ awk '$9 < 0.05 {print}' ls_assoc_f.assoc > select_ls_f.txt
 awk '$9 < 0.05 {print}' ls_assoc_m.assoc > select_ls_m.txt
 
 ## binary variable right slope of the weight, coded as control = 1 (coeff<-0.12), case = 2 (coeff>-0.12)
-plink --ped IBCqc_f.ped --map IBCqc.map --pheno rsweight_data.txt --pheno-name rs_bi --assoc --out rs_assoc_m
+plink --ped IBCqc_f.ped --map IBCqc.map --pheno rsweight_data.txt --pheno-name rs_bi --assoc --out rs_assoc_f
 plink --ped IBCqc_m.ped --map IBCqc.map --pheno rsweight_data.txt --pheno-name rs_bi --assoc --out rs_assoc_m
 
 ## keep the markers (rs of weight) with P<0.05 in females and males
