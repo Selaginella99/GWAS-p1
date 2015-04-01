4fb1c669c8aa48ae1f7c4cad35d3d4a338540df1
Selaginella_V <veronica9907@sina.com>
2015-04-01 17:39:57 -0400

    Update assoc_ibc.sh

--- assoc_ibc.sh
+++ assoc_ibc.sh
@@ -27,3 +27,21 @@ plink --ped IBCqc_m.ped --map IBCqc.map --pheno rsweight_data.txt --pheno-name r
 ## keep the markers (rs of weight) with P<0.05 in females and males
 awk '$9 < 0.05 {print}' rs_assoc_f.assoc > select_rs_f.txt
 awk '$9 < 0.05 {print}' rs_assoc_m.assoc > select_rs_m.txt
+
+
+### R coding for recoding the phenotype data
+lifespan=read.csv("Z:/Project/Framingham/Data/Phenotype/FHS/lifespan_data.csv",head=T,as.is=T)
+lifespan$ls_binary[(lifespan$lifespan>92)]<-2
+lifespan$ls_binary[(lifespan$lifespan<85)]<-1
+write.csv(lifespan,file="Z:/Project/Framingham/Data/Phenotype/FHS/lifespan_b_data.csv",row.names=F)
+
+rsweight=read.csv("Z:/Project/Framingham/Data/Phenotype/FHS/data/WEIGHT/RIGHT/Fhs22W4_all.csv",head=T,as.is=T)
+fid_id=read.csv("Z:/Project/Framingham/Data/Phenotype/FHS/data/WEIGHT/RIGHT/FID_idtype_FRAM_ver22.csv",head=T,as.is=T)
+fid_ids=fid_id[fid_id$ID%in%rsweight$ID,] ## get the sex variable
+rsweights=merge(rsweight,fid_ids)
+#head(rsweights)
+rsweights$rsw_bi[rsweights$age1<(-0.12)&rsweights$Sex==1]<-1
+rsweights$rsw_bi[rsweights$age1>(-0.12)&rsweights$Sex==1]<-2
+rsweights$rsw_bi[rsweights$age1<(-0.15)&rsweights$Sex==2]<-1
+rsweights$rsw_bi[rsweights$age1>(-0.15)&rsweights$Sex==2]<-2
+write.csv(rsweights,file="Z:/Project/Framingham/Data/Phenotype/FHS/data/WEIGHT/RIGHT/rsweight.csv",row.names=F)
