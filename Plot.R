d3efc38473a983453568557f6d36052c1f98f1fd
Selaginella_V <veronica9907@sina.com>
2015-04-09 14:07:19 -0400

    Update Plot.R

--- Plot.R
+++ Plot.R
@@ -1,4 +1,4 @@
-How to [set the interval](http://stackoverflow.com/questions/15717545/set-the-intervals-of-x-axis-using-r) by R
+How to set the interval by R? http://stackoverflow.com/questions/15717545/set-the-intervals-of-x-axis-using-r
 
 plot(0:23, d, type='b', axes=FALSE)
 axis(side=1, at=c(0:23))
