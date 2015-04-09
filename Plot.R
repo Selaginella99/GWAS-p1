0e70628eb7810414006cc0fa85a2780b8a4c10a2
Selaginella_V <veronica9907@sina.com>
2015-04-09 14:03:53 -0400

    Create Plot.R

---
+++ Plot.R
@@ -0,0 +1,6 @@
+How to [set the interval](http://stackoverflow.com/questions/15717545/set-the-intervals-of-x-axis-using-r) by R
+
+plot(0:23, d, type='b', axes=FALSE)
+axis(side=1, at=c(0:23))
+axis(side=2, at=seq(0, 600, by=100))
+box()
